36ca05ea816043f220699252245a0476e924d0d0
Andy Yuan <andyuan997@gmail.com>
2025-05-15 16:04:32 +0800

    樹懶開發主頁

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Projects", href: "/projects" },
-  { name: "Contact", href: "/contact" },
+  { name: "應用程式", href: "/contact" },
+  { name: "客戶案例", href: "/projects" },
+  { name: "聯絡我們", href: "/contact" },
 ];
 
 export default function Home() {
@@ -29,20 +30,13 @@ export default function Home() {
         quantity={100}
       />
       <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+        slothdevelop
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+          你懶得做，我們懶得廢話，樹懶程式開發團隊幫助有需求的你，當一隻樹懶就好。
         </h2>
       </div>
     </div>
